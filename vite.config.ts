ee73e6f2165df4712fdefa99681cc2214d2402ae
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-29 07:31:46 -0300

    chore: config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 
-export default defineConfig({
-  build: {
-    sourcemap: true
-  },
-  plugins: [tsconfigPaths(), react(), svgr()]
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return {
+    base: '/',
+    server: {
+      port: Number(process.env.VITE_PORT)
+    },
+    build: {
+      sourcemap: true
+    },
+    plugins: [tsconfigPaths(), react(), svgr()]
+  }
 })
