f285d8eb4ebd10a73be463ebe6d51005a5241cdb
Éverton Toffanetto <evertondgn@hotmail.com>
2022-05-30 19:16:07 -0300

    chore: disabled sourceMap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
       port: Number(process.env.VITE_PORT)
     },
     build: {
-      sourcemap: true
+      sourcemap: false
     },
     plugins: [tsconfigPaths(), react(), svgr()]
   }
