06787691db5409e7632e33be2a2c1acca51cb2b8
Éverton Toffanetto <evertondgn@hotmail.com>
2022-08-02 17:53:14 -0300

    refactor: separated type ErrorApi to file

--- src/infra/http/getClient/index.ts
+++ src/infra/http/getClient/index.ts
@@ -1,7 +1,7 @@
 import { httpClientAdapter } from 'infra/adapters/httpClient'
 import { errorApi } from 'data'
 import { GetClientType } from './types'
-import { ErrorApi } from 'infra/adapters/httpClient/types'
+import { ErrorApi } from 'data/errorApi/types'
 
 export const getClient = async (url: string): Promise<GetClientType> => {
   try {
