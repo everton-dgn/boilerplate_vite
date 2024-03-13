2bffd5cea819fdf016eea3c45f7af269068dc546
Éverton Toffanetto <evertondgn@hotmail.com>
2024-03-13 16:42:24 -0300

    fix: fix theme

--- src/theme/utilities/fontWeight.ts
+++ src/theme/utilities/fontWeight.ts
@@ -5,7 +5,7 @@ import { fontWeights } from '../variables'
 export const fontWeight = plugin(({ addUtilities }) => {
   const fw: Record<string, { fontWeight: string }> = {}
   Object.entries(fontWeights).forEach(([key, value]) => {
-    fw[`.fs-${key}`] = { fontWeight: `${value}` }
+    fw[`.fw-${key}`] = { fontWeight: `${value}` }
   })
   addUtilities(fw)
 })
