66c64b73de0b99f82207581fd175f38bd436de78
Éverton Toffanetto <evertondgn@hotmail.com>
2024-05-10 00:40:10 -0300

    Update .eslintcache file
    The .eslintcache has been updated for better accuracy in tracking the ESLint analysis. This will improve the linting process and code standards throughout the project.

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -5,11 +5,11 @@ const Error = () => {
 
   return (
     <div className="min-h-screen p-16 center col-full">
-      <header className="fw-600 mb-32 fs-20" role="banner">
+      <header className="mb-32 fw-600 fs-20" role="banner">
         Error!
       </header>
       <main className="ai-center col-full g-20">
-        <h1 className="fw-500 text-center fs-32">Page not found!</h1>
+        <h1 className="text-center fw-500 fs-32">Page not found!</h1>
       </main>
     </div>
   )
